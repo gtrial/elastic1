9fa133bab66cc2edb424601eabe128a9df5962a0
KYIV\dmyk <dmytro.kostiuchenko@247e.com>
2017-10-24 13:43:45 +0300

    Initial commit

---
+++ elastic1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Elasticsearch.Net;
+
+namespace elastic1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var settings = new ConnectionConfiguration(new Uri("http://127.0.0.1:9200")).RequestTimeout(TimeSpan.FromMinutes(2));
+            var lowlevelClient = new ElasticLowLevelClient(settings);
+        }
+    }
+}
